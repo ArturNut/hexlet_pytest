b2b16ca3fd30dc8e27d70f63670895b029ef4ef0
Artur Nutfullin <artur_nutfullin@mail.ru>
2024-01-21 16:23:47 +0700

    editing by flake8

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 def test_stack():
     stack = []
     # Добавляем два элемента в стек и затем извлекаем их
@@ -27,6 +28,7 @@ def test_stack2():
     stack.pop()
     assert stack.pop() == 'one'
 
+
 def test_emptiness():
     stack = []
     assert not stack
@@ -36,10 +38,11 @@ def test_emptiness():
     stack.pop()
     assert not stack
 
+
 def test_pop_with_empty_stack():
     stack = []
-    # проверить что вызывается конкретное исключение можно с помощью конструкции with pytest.raises()
+    # проверить что вызывается конкретное исключение
+    # можно с помощью конструкции with pytest.raises()
     # если внутри блока вызовется исключение, то тест будет пройден
     with pytest.raises(IndexError):
         stack.pop()
-
